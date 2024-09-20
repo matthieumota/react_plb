9f2d03dc4264da17026777d138c409962990a7f8
Matthieu Mota <matthieumota@gmail.com>
2024-09-20 11:29:09 +0200

    integration

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,19 +1,23 @@
 import Ajax from './components/Ajax'
 import { NavLink, Outlet } from 'react-router-dom'
+import MenuLink from './components/MenuLink'
 
 function App() {
   return (
     <>
-      <nav>
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/a-propos">A propos</NavLink>
-        <NavLink to="/exercices/lifecycle">Exercices Lifecycle</NavLink>
-        <NavLink to="/exercices/event-listener">Exercices Event listener</NavLink>
-      </nav>
+      <div className="bg-slate-600">
+        <nav className="max-w-screen-xl mx-auto mb-8">
+          <MenuLink to="/">Accueil</MenuLink>
+          <MenuLink to="/films">Films</MenuLink>
+          <MenuLink to="/a-propos">A propos</MenuLink>
+          <MenuLink to="/exercices/lifecycle">Exercices Lifecycle</MenuLink>
+          <MenuLink to="/exercices/event-listener">Exercices Event listener</MenuLink>
+        </nav>
+      </div>
 
-      <Outlet /> {/* Charge le bon élément en fonction de la page */}
-
-      <Ajax />
+      <div className="max-w-screen-xl mx-auto">
+        <Outlet /> {/* Charge le bon élément en fonction de la page */}
+      </div>
     </>
   )
 }
